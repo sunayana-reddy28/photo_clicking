c152b8c7c7f1ad1f0da82f5d4562b74b64e40f45
sunayana-reddy28 <sanagasunayanareddy@gmail.com>
2024-08-24 12:42:18 -0400

    files

---
+++ style.js
@@ -0,0 +1,8 @@
+function entered(){
+    document.getElementById("c1").style.backgroundColor=" #7083d8";
+}
+
+function leaving(){
+    document.getElementById("c1").style.backgroundColor="#ff69b4";
+
+}
